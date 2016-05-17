1c35c18771ccf6b7a8490761a151e6a693b45660
jordane-quincy <jordanequincy@gmail.com>
2016-05-17 13:42:00 +0200

    suppr de code en commentaire

--- BaseDeTravail/serverless-webrtc (2)/serverless-webrtc/js/serverless-webrtc.js
+++ BaseDeTravail/serverless-webrtc (2)/serverless-webrtc/js/serverless-webrtc.js
@@ -53,68 +53,6 @@ function getSmileyDom(text,  imgLink, hasOnClickFunction){
   return '<img src="'+ imgLink +'" title="'+text+'" alt="'+text+'" '+ onClickFunction +' />';
 }
 
-//partie video
-/*
-var video = $('#videoWebcam');
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
-
-function handleVideo(stream) {
-  console.log("handleVideo ");
-    video.src = window.URL.createObjectURL(stream);
-}
-
-function videoError(e) {
-    console.log("videoError "+ e);
-}
-*/
-/*
-var getUserMedia = require('getusermedia');
-var attachMediaStream = require('attachmediastream');
-
-// get user media
-getUserMedia(function (err, stream) {
-    // if the browser doesn't support user media
-    // or the user says "no" the error gets passed
-    // as the first argument.
-    if (err) {
-      console.log('failed');
-    } else {
-      console.log('got a stream', stream);
-
-      // attaches a stream to an element (it returns the element)
-      var videoEl = attachMediaStream(stream, document.getElementById('videoWebcam'));
-
-      // if you don't pass an element it will create a video tag
-      var generatedVideoEl = attachMediaStream(stream);
-
-      // you can also pass options
-      var videoEl = attachMediaStream(stream, someEl, {
-        // this will set the autoplay attribute on the video tag
-        // this is true by default but you can turn it off with this option.
-        autoplay: true,
-
-        // let's you mirror the video. It's false by default, but it's common
-        // to mirror video when showing a user their own video feed.
-        // This makes that easy.
-        mirror: true,
-
-        // muted is false, by default
-        // this will mute the video. Again, this is a good idea when showing
-        // a user their own video. Or there will be feedback issues.
-        muted: true,
-
-        // attach as an audio element instead of video
-        audio: false
-      });
-
-    }
-});
-*/
-
 var cfg = {"iceServers":[{"url":"stun:23.21.150.121"}]},
     con = { 'optional': [{'DtlsSrtpKeyAgreement': true}] };
 
@@ -147,10 +85,6 @@ $('#createBtn').click(function() {
     createLocalOffer();
 });
 
-/*
-$('#joinBtn').click(function() {
-    $('#getRemoteOffer').modal('show');
-});*/
 $('#joinBtn').click(function () {
   navigator.getUserMedia = navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
